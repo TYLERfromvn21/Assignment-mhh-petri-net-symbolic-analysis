1b032c6b4e14d02047db8916774a86f197c3f72e
Trần Quốc Duy <putin21062005@gmail.com>
2025-11-11 16:33:11 +0700

    thêm task templates

---
+++ src/task3_symbolic_bdd.py
@@ -0,0 +1,8 @@
+"""
+TASK 3: Symbolic Reachability (BDD)
+"""
+from dd.autoref import BDD
+
+def bdd_reachability(net):
+    # TODO: Implement BDD here
+    pass
---
+++ src/task4_deadlock_ilp.py
@@ -0,0 +1,8 @@
+"""
+TASK 4: Deadlock Detection (ILP + BDD)
+"""
+import pulp
+
+def detect_deadlock(net):
+    # TODO: Implement ILP here
+    pass
